aaee5947231e6071b048a9cff7d926aeae29612c
markus <mararon@student.chalmers.se>
2014-09-22 11:53:04 +0200

    I think adding Snd to Signal is necessary, since it's used in StructS (not sure)

--- Frontend/Temp.hs
+++ Frontend/Temp.hs
@@ -8,41 +8,55 @@
 
 {-# LANGUAGE InstanceSigs #-}
 
-module Temp where
+module Frontend.Temp where
+
+import Core
+import Expr
+
+import Frontend.Stream (Stream)
+import qualified Frontend.Stream as S
+
+import Control.Applicative
+import Control.Monad
+import Control.Monad.Operational
+import Data.Typeable
 
 import Prelude hiding (zip, fst, snd)
 import qualified Prelude as P
 
 --------------------------------------------------------------------------------
+-- * Signals
+--------------------------------------------------------------------------------
 
 data Leaf a
 
-data Expr a
-
-data Stream a
-
 data Signal a
   where
-    Const :: Stream a -> Signal (Leaf a)
-    Lift  :: (Stream a -> Stream b) -> Signal (Leaf a) -> Signal (Leaf a)
-    Map   :: (Struct a -> Struct b) -> Signal a -> Signal b
+    Const :: (Stream a)                                -> Signal (Leaf a)
+    Lift  :: (Stream a -> Stream b) -> Signal (Leaf a) -> Signal (Leaf b)
+    Map   :: (Struct a -> Struct b) -> Signal a        -> Signal b
 
     Zip   :: Signal a -> Signal b -> Signal (a, b)
     Fst   :: Signal (a, b) -> Signal a
+    Snd   :: Signal (a, b) -> Signal b
+  deriving Typeable
 
-newtype Sig a = Sig { unSig :: Signal (Leaf a)}
+newtype Sig a = Sig { unSig :: Signal (Leaf a) }
 
-zip :: Signal a -> Signal b -> Signal (a, b)
-zip = Zip
-
-fst :: Signal (a, b) -> Signal a
-fst = Fst
-
-snd :: Signal (a, b) -> Signal b
-snd = undefined -- fst . map swap
+liftS :: (Stream a -> Stream b) -> Signal (Leaf a) -> Signal (Leaf b)
+liftS = Lift
 
 mapS :: (Struct a -> Struct b) -> Signal a -> Signal b
 mapS = Map
+
+zipS :: Signal a -> Signal b -> Signal (a, b)
+zipS = Zip
+
+fstS :: Signal (a, b) -> Signal a
+fstS = Fst
+
+sndS :: Signal (a, b) -> Signal b
+sndS = Snd
 
 --------------------------------------------------------------------------------
 
@@ -71,8 +85,8 @@ instance (StructS a, StructS b) => StructS (a, b)
   where
     type Internal (a, b) = (Internal a, Internal b)
 
-    fromS (a, b) = zip (fromS a) (fromS b)
-    toS p = (toS (fst p), toS (snd p))
+    fromS (a, b) = zipS (fromS a) (fromS b)
+    toS p        = (toS (fstS p), toS (sndS p))
 
 --------------------------------------------------------------------------------
 
@@ -112,7 +126,7 @@ lift
   => (Normal (Internal s1) -> Normal (Internal s2)) -> s1 -> s2
 lift f = toS . mapS (toE . f . fromE) . fromS
 
---------------------------------------------------------------------------------
+----------------------------------------
 
 addE :: Expr Int -> Expr Int -> Expr Int
 addE = undefined
@@ -125,3 +139,7 @@ addS' = lift $ uncurry addE
 
 addS'' :: (Sig (Expr Int), Sig (Expr Int)) -> Sig (Expr Int)
 addS'' = lift $ uncurry addE
+
+--------------------------------------------------------------------------------
+-- * Observable
+--------------------------------------------------------------------------------
