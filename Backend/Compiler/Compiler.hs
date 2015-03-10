139a6e14817c08f07daf25476bae58803f5ac7a9
markus-bit <mararon@chalmers.se>
2015-03-10 08:31:23 +0100

    added buffers

--- Backend/Compiler/Compiler.hs
+++ Backend/Compiler/Compiler.hs
@@ -20,7 +20,7 @@ import qualified Frontend.Stream as Str
 import           Frontend.Signal (Signal, Sig, Struct(..), TStruct(..), Empty)
 import qualified Frontend.Signal as S
 
-import Frontend.SignalObsv (TSignal(..), Node)
+import Frontend.SignalObsv (TSignal(..), Node, edges)
 
 import Backend.Ex
 import Backend.Compiler.Cycles
@@ -34,7 +34,7 @@ import Control.Monad.Operational
 import Data.Typeable
 import Data.Reify (Unique, Graph(..), reifyGraph)
 import Data.Maybe (fromJust)
-import Data.List  (sortBy)
+import Data.List  (sortBy, mapAccumR)
 import Data.Function (on)
 
 import           Data.Map (Map, (!))
@@ -128,14 +128,6 @@ read_in u _ =
        Just s  -> lift $ reads s
        Nothing -> error "hepa: type error"
 
--- | Read 
-read_out :: Typeable a => Unique -> TStruct exp a -> Type exp (Struct exp a)
-read_out u _ =
-  do (Ex ch) <- asks ((! u) . _ch_out . _channels)
-     case gcast ch of
-       Just s  -> lift $ reads s
-       Nothing -> error "!"
-
 -- | Write
 write_out :: Typeable a => Unique -> Struct exp a -> Type exp ()
 write_out u s =
@@ -197,8 +189,9 @@ compiler' nodes links order input = Str.stream $
      env <- init (Str.run input)
      return $
        do let t = undefined :: TStruct exp (Empty (exp b))
-          (Leaf value) <- run env (mapM_ compile sorted >> read_out last t)
+          (Leaf value) <- run env (mapM_ compile sorted >> read_in last t)
           return value
+
   where
     run :: Enviroment Unique exp -> Type exp x -> Prog exp x
     run = flip runReaderT
@@ -249,31 +242,85 @@ compiler f =
        True  -> error "found cycle in graph"
        False -> compiler' nodes links order
 
+--------------------------------------------------------------------------------
+-- * Buffers
+--------------------------------------------------------------------------------
+
+data Buffer exp a = Buffer
+  { getBuff :: exp Int -> Program (CMD exp) (exp a)
+  , putBuff :: exp a   -> Program (CMD exp) ()
+  }
+
+newBuff
+  :: forall exp a. (Num (exp Int), Integral (exp Int))
+  => exp Int -> exp a -> Program (CMD exp) (Buffer exp a)
+newBuff size init =
+  do arr <- C.newArr size init
+     ir  <- C.newRef (0 :: exp Int)
+     let get j = do i <- C.unsafeGetRef ir
+                    C.getArr ((i + (size - j - 1)) `mod` size) arr
+     let put a = do i <- C.unsafeGetRef ir
+                    C.setArr i a arr
+                    -- missing iff
+     return $ Buffer get put
+
+--------------------------------------------------------------------------------
 
 
+opt_delay_chains :: (Typeable e, Typeable a, Num (e Int))
+  => Map Unique (Node e)
+  -> ( Map Unique (Node e)
+     , Map Unique [e a])
+opt_delay_chains nodes =
+  let x = find_chains nodes
+      (y, buffs) = buffer_chains x
+      z = merge_chains nodes y
+  in  (z, buffs)
 
+find_chains :: Map Unique (Node e) -> Map Unique [(Unique, Node e)]
+find_chains nodes =
+  let delays = M.foldrWithKey (\k n -> M.insert (edge n) (k, n)) M.empty
+             $ M.filter isDelay nodes
+      heads  = M.foldr (M.delete . fst) delays delays
+  in  M.map (flip chain delays) heads
+  where
+    chain :: (Unique, Node e) -> Map Unique (Unique, Node e) -> [(Unique, Node e)] 
+    chain v@(i, _) m = v : maybe [] (flip chain m) (M.lookup i m)
 
+    edge :: Node e -> Unique
+    edge = head . edges
 
+    isDelay :: Node e -> Bool
+    isDelay (TDelay {}) = True
+    isDelay _           = False
+  
+buffer_chains
+  :: forall e a. (Typeable e, Typeable a, Num (e Int))
+  => Map Unique [(Unique, Node e)]
+  -> ( Map Unique [(Unique, Node e)]
+     , Map Unique [e a])
+buffer_chains chains =
+  let vals  = M.map (map val) chains
+      buffs = M.mapWithKey (\k -> snd . mapAccumR (acc k) 1) chains
+  in (buffs, vals)
+  where
+    val     (i, TDelay v _) = fromJust $ cast v
+    acc k n (i, TDelay v _) = (n+1, (i, TDBuff k $ fromIntegral n))
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+merge_chains
+  :: Map Unique (Node e)
+  -> Map Unique [(Unique, Node e)]
+  -> Map Unique (Node e)
+merge_chains nodes chains =
+  let nodes' = M.foldr (flip $ foldr (\(u,n) -> M.adjust (const n) u)) nodes chains
+      heads' = M.foldrWithKey (\k _ -> M.insert (-k) (TVBuff k)) M.empty chains
+  in  M.union nodes' heads'
 
 --------------------------------------------------------------------------------
 -- * Testing
 --------------------------------------------------------------------------------
 
-inspect_compiler :: (Typeable exp, Typeable a, Typeable b)
+inspect_compiler :: (Typeable exp, Typeable a, Typeable b, Num (exp Int))
                  =>    (Sig exp a -> Sig exp b)
                  -> IO (Str exp a -> Str exp b)
 inspect_compiler f =
@@ -283,6 +330,8 @@ inspect_compiler f =
          order = sorter root nodes
          cycle = cycles root nodes
 
+   --let chains = M.map (const "Buff.") $ snd $ opt_delay_chains $ M.fromList nodes
+     
      putStrLn "=================================================="
      putStrLn "= Inspecting Compiler"
      putStrLn "=================================================="
@@ -302,6 +351,13 @@ inspect_compiler f =
      putStrLn "--------------------------------------------------"
      putStrLn $ show $ _output links
      putStrLn "--------------------------------------------------"
+
+{-
+     putStrLn "- Buffers"
+     putStrLn "--------------------------------------------------"
+     putStrLn $ show $ chains
+     putStrLn "--------------------------------------------------"
+-}
      
      return $ case cycle of
        True  -> error "found cycle in graph"
