72f9f6aa01da3d65876401f2a008c2d9541b5e8b
Markus <mararon@chalmers.se>
2015-05-11 16:51:46 +0200

    working on simpler signals

--- Frontend/Signal.hs
+++ Frontend/Signal.hs
@@ -12,15 +12,19 @@ module Frontend.Signal where
 import Core
 import Backend.Nested
 
-import           Frontend.Stream (Stream, Str)
+import           Frontend.Stream (Stream(..), Str)
 import qualified Frontend.Stream as S
 
+import Data.Functor.Identity
 import Data.Dynamic (Dynamic)
+import Data.Proxy   (Proxy(..))
 import Prelude ( Eq, Show, String, ($), (.), id 
                , Num,        (+), (-), (*), fromInteger, abs, signum
                , Fractional, (/), fromRational, recip
                , Floating,   (**), pi, sin
                , curry, uncurry
+               , fmap
+               , fst, snd
                , undefined, error
                )
 
@@ -31,254 +35,249 @@ import Prelude ( Eq, Show, String, ($), (.), id
 data Signal (instr :: (* -> *) -> * -> *) (a :: *)
   where
     -- ^ ...
-    Const :: (Typeable a, IPred instr a)
+    Const :: (Typeable a, VarPred (IExp instr) a)
           => Stream instr (IExp instr a)
-          -> Signal instr (Empty instr a)
+          -> Signal instr (Identity   a)
 
-    -- ^ ...
-    Lift  :: (Typeable a, Typeable b, IPred instr a, IPred instr b)
-          => (Stream instr (IExp instr a) -> Stream instr (IExp instr b))
-          -> Signal instr (Empty instr a)
-          -> Signal instr (Empty instr b)
-
-    Delay :: (Typeable a, IPred instr a)
-          => IExp   instr a
-          -> Signal instr (Empty instr a)
-          -> Signal instr (Empty instr a)
-
-    -- ^ ...
-    Map   :: ( Typeable a, Typeable b
-             , Instr a ~ instr, Instr b ~ instr
-             , Rep a, Rep b
+    Map   :: ( Typeable a
+             , Typeable b
              )
-          => (Tuple instr a -> Tuple instr b)
+          => (Stream instr a -> Stream instr b)
           -> Signal instr a
           -> Signal instr b
 
     -- ^ ...
-    Join  :: ( Typeable a, Typeable b
-             , Instr a ~ instr, Instr b ~ instr
-             , Rep a, Rep b
+    Join  :: ( Witness a, Typeable a
+             , Witness b, Typeable b
              )
           => Signal instr a
           -> Signal instr b
           -> Signal instr (a, b)
 
     -- ^ ...
-    Left  :: ( Typeable a, Typeable b
-             , Instr a ~ instr, Instr b ~ instr
-             , Rep a, Rep b
+    Left  :: ( Witness a, Typeable a
+             , Witness b, Typeable b
              )
           => Signal instr (a, b)
           -> Signal instr a
 
     -- ^ ...
-    Right :: ( Typeable a, Typeable b
-             , Instr a ~ instr, Instr b ~ instr
-             , Rep a, Rep b
+    Right :: ( Witness a, Typeable a
+             , Witness b, Typeable b
              )
           => Signal instr (a, b)
           -> Signal instr b
 
-    -- ^ ...
-    Var   :: (Typeable a, Instr a ~ instr, Rep a)
-          => Dynamic
-          -> Signal instr a
-
 -- | `Shorthand` for signals which produce values of type `exp a`
-newtype Sig instr a = Sig { unSig :: Signal instr (Empty instr a) }
+newtype Sig instr a = Sig { runSig :: Signal instr (Identity a) }
 
 --------------------------------------------------------------------------------
+-- **
 
-repeat :: ( e ~ IExp instr, Typeable e
-          , IPred instr a, Typeable a
-          )
-       => e a -> Sig instr a
-repeat = Sig . Const . S.repeat
+lift0 :: (e ~ IExp i, VarPred e a, Typeable a) => e a -> Sig i a
+lift0 = Sig . Const . S.repeat
 
-map :: ( e ~ IExp instr, Typeable e
-       , IPred instr a, Typeable a
-       , IPred instr b, Typeable b
-       )
-    => (e a -> e b) -> Sig instr a -> Sig instr b
-map f = Sig . Lift (S.map f) . unSig
-
-zipWith :: (e ~ IExp instr, Typeable e, Typeable instr
-           , IPred instr a, Typeable a
-           , IPred instr b, Typeable b
-           , IPred instr c, Typeable c
-           )
-        => (e a -> e b -> e c) -> Sig instr a -> Sig instr b -> Sig instr c
-zipWith f = curry $ lift $ uncurry f
-
---------------------------------------------------------------------------------
-
-delay :: ( e ~ IExp instr, Typeable e
-         , IPred instr a, Typeable a
-         )
-      => e a -> Sig instr a -> Sig instr a
-delay a = Sig . Delay a . unSig
-
---------------------------------------------------------------------------------
-
-instance ( Num (IExp instr a)
-         , IPred instr a
-         , Typeable (IExp instr)
-         , Typeable instr
-         , Typeable a
-         ) =>
-    Num (Sig instr a)
-  where
-    fromInteger = repeat . fromInteger
-    (+)         = zipWith (+)
-    (*)         = zipWith (*)
-    (-)         = zipWith (-)
-
-    abs = todo; signum = todo;
-
-instance ( Fractional (IExp instr a)
-         , IPred instr a
-         , Typeable (IExp instr)
-         , Typeable instr
-         , Typeable a
-         ) =>
-    Fractional (Sig instr a)
-  where
-    fromRational = repeat . fromRational
-    (/)          = zipWith (/)
-
-    recip = todo;
-
-todo = error "unsupported operation"
+lift1 :: forall i e a b. (e ~ IExp i) => (e a -> e b) -> Sig i a -> Sig i b
+lift1 f a = undefined
 
 --------------------------------------------------------------------------------
 -- * Nested Signals
 --------------------------------------------------------------------------------
 
-type family Instr  a :: (* -> *) -> * -> *
+data Wit a
+  where
+    WE :: (Typeable a)
+       => Wit (Identity a)
+       
+    WP :: ( Witness a, Typeable a
+          , Witness b, Typeable b
+          )
+       => Wit a -> Wit b -> Wit (a, b)
+
+class Witness a
+  where
+    wit :: Wit a
+
+instance Typeable a => Witness (Identity a)
+  where
+    wit = WE
+
+instance forall a b.
+    ( Witness a, Typeable a
+    , Witness b, Typeable b
+    )
+    => Witness (a, b)
+  where
+    wit = WP (wit :: Wit a) (wit :: Wit b)
+
+--------------------------------------------------------------------------------
+-- **
+
+type family Packed (i :: (* -> *) -> * -> *) a :: *
+type instance Packed i (Identity a) = Signal i (Identity a)
+type instance Packed i (a, b)       = (Packed i a, Packed i b)
+
+pack :: forall i a. Witness a => Signal i a -> Packed i a
+pack s = go (wit :: Wit a) s
+  where
+    go :: Wit a -> Signal i a -> Packed i a
+    go (WE)     s = s
+    go (WP l r) s = (pack (Left s), pack (Right s))
+    
+unpack :: forall i a. Witness a => Packed i a -> Signal i a
+unpack s = go (wit :: Wit a) s
+  where
+    go :: Wit a -> Packed i a -> Signal i a
+    go (WE)     s = s
+    go (WP l r) s = Join (unpack (fst s)) (unpack (snd s))
+
+--------------------------------------------------------------------------------
+-- **
+
+type family Wrapped a :: *
+type instance Wrapped (Identity a) = a
+type instance Wrapped (a, b)       = (Wrapped a, Wrapped b)
+
+wrap :: forall a. Witness a => a -> Wrapped a
+wrap w = go (wit :: Wit a) w
+  where
+    go :: Wit a -> a -> Wrapped a
+    go (WE)     a = runIdentity a
+    go (WP l r) a = (wrap (fst a), wrap (snd a))
+
+unwrap :: forall a. Witness a => Wrapped a -> a
+unwrap w = go (wit :: Wit a) w
+  where
+    go :: Wit a -> Wrapped a -> a 
+    go (WE)     a = Identity a
+    go (WP l r) a = (unwrap (fst a), unwrap (snd a))
+
+--------------------------------------------------------------------------------
+-- **
+
+lift :: forall proxy i a b.
+       ( Witness a, Typeable a
+       , Witness b, Typeable b
+       )
+    => proxy i a b
+    -> (Wrapped  a -> Wrapped  b)
+    -> (Packed i a -> Packed i b)
+lift _ f = pack . Map (stream f :: Stream i a -> Stream i b) . unpack
+
+stream :: forall i a b.
+    ( Witness a
+    , Witness b
+    )
+    => (Wrapped  a -> Wrapped  b)
+    -> (Stream i a -> Stream i b)
+stream f (Stream s) = Stream $ fmap (fmap (unwrap . f . wrap)) s
+
+--------------------------------------------------------------------------------
+
+{-
+zipWith :: (a -> b -> c) -> Signal a -> Signal b -> Signal c
+zipWith f = curry $ liftAPA $ uncurry f
+
+liftAPA :: ((a, b) -> c) -> (Signal a, Signal b) -> Signal c
+liftAPA = undefined
+-}
+--------------------------------------------------------------------------------
+{-
+type family Instr a :: (* -> *) -> * -> *
 type instance Instr (Sig    instr a) = instr
 type instance Instr (Signal instr a) = instr
 type instance Instr (Empty  instr a) = instr
 type instance Instr (a, b)           = Instr a
 
 -- | Conversion between signals and tuples
-class SSignal a
+class Syntax a
   where
     type Internal a :: *
     
-    sugarS   :: a -> Signal (Instr a) (Internal a)
-    desugarS :: Signal (Instr a) (Internal a) -> a
-
--- | Conversion betwee instructions and tuples
-class STuple a
-  where
-    type Normal a :: *
-    
-    sugarE   :: Normal a -> Tuple (Instr a) a 
-    desugarE :: Tuple (Instr a) a -> Normal a
+    sugar   :: a -> Signal (Instr a) (Internal a)
+    desugar :: Signal (Instr a) (Internal a) -> a
 
 --------------------------------------------------------------------------------
 
-instance SSignal (Sig instr a)
+instance Syntax (Sig instr a)
   where
     type Internal (Sig instr a) = Empty instr a
 
-    sugarS   = unSig
-    desugarS = Sig
+    sugar   = unSig
+    desugar = Sig
 
-instance SSignal (Signal instr (Empty instr a))
+instance Syntax (Signal instr (Empty instr a))
   where
     type Internal (Signal instr (Empty instr a)) = Empty instr a
 
-    sugarS   = id
-    desugarS = id
+    sugar   = id
+    desugar = id
 
-instance ( SSignal a, SSignal b
+instance ( Syntax a
+         , Syntax b
+
          , Instr a ~ Instr b
          , Instr a ~ Instr (Internal a)
          , Instr b ~ Instr (Internal b)
-
+{-
          , Rep (Internal a)
          , Rep (Internal b)
-           
+-}           
          , Typeable (IExp (Instr a))
          , Typeable (IExp (Instr b))
          , Typeable (Internal a)
          , Typeable (Internal b)
          )
-    => SSignal (a, b)
+    => Syntax (a, b)
   where
     type Internal (a, b) = (Internal a, Internal b)
 
-    sugarS   (a, b) = Join (sugarS a) (sugarS b)
-    desugarS s      = (desugarS (Left s), desugarS (Right s))
-
---------------------------------------------------------------------------------
-
-instance (IPred instr a, Typeable a) => STuple (Empty instr a)
-  where
-    type Normal (Empty instr a) = IExp instr a
-
-    sugarE            = Leaf
-    desugarE (Leaf a) = a
-
-instance (STuple a, STuple b, Instr a ~ Instr b) => STuple (a, b)
-  where
-    type Normal (a, b) = (Normal a, Normal b)
-
-    sugarE   (a, b)       = Branch (sugarE a) (sugarE b)
-    desugarE (Branch a b) = (desugarE a, desugarE b)
+    sugar   (a, b) = Join (sugar a) (sugar b)
+    desugar s      = (desugar (Left s), desugar (Right s))
 
 --------------------------------------------------------------------------------
 -- **
 
 -- | ...
-lift :: ( SSignal a
-        , SSignal b
-        , Instr a ~ Instr b
-          
-        , STuple (Internal a)
-        , STuple (Internal b)
-        , Instr  (Internal a) ~ Instr a
-        , Instr  (Internal b) ~ Instr b
+lift :: ( Syntax a
+        , Syntax b
 
-        , Rep (Internal a)
-        , Rep (Internal b)
-          
+        , Instr a ~ Instr b
+        , Instr (Internal a) ~ Instr a
+        , Instr (Internal b) ~ Instr b
+
         , Typeable (Internal a)
         , Typeable (Internal b)
-        , Typeable (IExp (Instr (Internal a)))
-        , Typeable (IExp (Instr (Internal b)))
         )
-     => (Normal (Internal a) -> Normal (Internal b)) -> a -> b
-lift f = desugarS . Map (sugarE . f . desugarE) . sugarS
-
+     => (Internal a -> Internal b) -> a -> b
+lift f = desugar . Map (S.lift1 f) . sugar
+-}
 --------------------------------------------------------------------------------
 -- *
 --------------------------------------------------------------------------------
-
+{-
 -- | ..
 class Rep a
   where
-    represent :: Tuple (Instr a) a -> Suple (Instr a) a
-
+    represent :: Nested (Instr a) a -> Suple (Instr a) a
+-}
 --------------------------------------------------------------------------------
-
-instance (IPred instr a, Typeable a) => Rep (Empty instr a)
+{-
+instance (VarPred (IExp instr) a, Typeable a) => Rep (Empty instr a)
   where
     represent _ = Seaf ""
 
 instance (Instr a ~ Instr b, Rep a, Rep b) => Rep (a, b)
   where
-    represent (_ :: Tuple (Instr a) (a, b)) = Sranch (represent left) (represent right)
+    represent (_ :: Nested (Instr a) (a, b)) = Sranch (represent left) (represent right)
       where
-        left  = undefined :: Tuple (Instr a) a
-        right = undefined :: Tuple (Instr b) b
-
+        left  = undefined :: Nested (Instr a) a
+        right = undefined :: Nested (Instr b) b
+-}
 --------------------------------------------------------------------------------
 -- ** 
-
+{-
 -- | ...
 rep :: forall c instr a. (Rep a, Instr a ~ instr) => c instr a -> Suple instr a
-rep _ = represent (undefined :: Tuple instr a)
-
+rep _ = represent (error "rep" :: Nested instr a)
+-}
