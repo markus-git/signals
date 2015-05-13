6ba20ac0b5a263b75c628769ff61d23a501b957b
Markus <mararon@chalmers.se>
2015-05-13 09:03:08 +0200

    It works! But I think I can do better yet..

--- Frontend/Signal.hs
+++ Frontend/Signal.hs
@@ -5,8 +5,6 @@
 {-# LANGUAGE FlexibleContexts    #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 
-{-# LANGUAGE UndecidableInstances #-} -- !
-
 module Frontend.Signal where
 
 import Core
@@ -35,16 +33,19 @@ import Prelude ( Eq, Show, String, ($), (.), id
 data Signal (instr :: (* -> *) -> * -> *) (a :: *)
   where
     -- ^ ...
-    Const :: (Typeable a, VarPred (IExp instr) a)
-          => Stream instr (IExp instr a)
-          -> Signal instr (Identity   a)
-
-    Map   :: ( Typeable a
-             , Typeable b
+    Const :: ( Typeable a, VarPred e a
+             , e ~ IExp instr
              )
-          => (Stream instr a -> Stream instr b)
-          -> Signal instr a
-          -> Signal instr b
+          => Stream instr (F e (Identity a))
+          -> Signal instr (Identity a)
+
+    -- ^ ...
+    Map   :: ( Typeable a --, VarPred (IExp instr) a
+             , Typeable b --, VarPred (IExp instr) b
+             , e ~ IExp instr
+             )
+          => (Stream instr (F e a) -> Stream instr (F e b))
+          -> (Signal instr a       -> Signal instr b)
 
     -- ^ ...
     Join  :: ( Witness a, Typeable a
@@ -68,6 +69,9 @@ data Signal (instr :: (* -> *) -> * -> *) (a :: *)
           => Signal instr (a, b)
           -> Signal instr b
 
+    -- ^ ...
+--  Var   :: (Witness a, Typeable a) => Dynamic -> Signal instr a
+
 -- | `Shorthand` for signals which produce values of type `exp a`
 newtype Sig instr a = Sig { runSig :: Signal instr (Identity a) }
 
@@ -77,8 +81,24 @@ newtype Sig instr a = Sig { runSig :: Signal instr (Identity a) }
 lift0 :: (e ~ IExp i, VarPred e a, Typeable a) => e a -> Sig i a
 lift0 = Sig . Const . S.repeat
 
-lift1 :: forall i e a b. (e ~ IExp i) => (e a -> e b) -> Sig i a -> Sig i b
-lift1 f a = undefined
+lift1 :: forall i e a b.
+       ( e ~ IExp i
+       , Witness a, Typeable a --, VarPred e a
+       , Witness b, Typeable b --, VarPred e b
+       )
+    => (e a -> e b) -> Sig i a -> Sig i b
+lift1 f = lift z f
+  where z = undefined :: proxy i (Identity a) (Identity b)
+
+lift2 :: forall i e a b c.
+       ( e ~ IExp i
+       , Witness a, Typeable a --, VarPred e a
+       , Witness b, Typeable b --, VarPred e b
+       , Witness c, Typeable c --, VarPred e c
+       )
+    => (e a -> e b -> e c) -> Sig i a -> Sig i b -> Sig i c
+lift2 f = curry $ lift z $ uncurry f
+  where z = undefined :: proxy i (Identity a, Identity b) (Identity c)
 
 --------------------------------------------------------------------------------
 -- * Nested Signals
@@ -86,13 +106,12 @@ lift1 f a = undefined
 
 data Wit a
   where
-    WE :: (Typeable a)
-       => Wit (Identity a)
-       
+    WE :: Typeable a => Wit (Identity a)
+          
     WP :: ( Witness a, Typeable a
           , Witness b, Typeable b
           )
-       => Wit a -> Wit b -> Wit (a, b)
+          => Wit a -> Wit b -> Wit (a, b)
 
 class Witness a
   where
@@ -114,170 +133,47 @@ instance forall a b.
 -- **
 
 type family Packed (i :: (* -> *) -> * -> *) a :: *
-type instance Packed i (Identity a) = Signal i (Identity a)
+type instance Packed i (Identity a) = Sig i a
 type instance Packed i (a, b)       = (Packed i a, Packed i b)
 
 pack :: forall i a. Witness a => Signal i a -> Packed i a
 pack s = go (wit :: Wit a) s
   where
     go :: Wit a -> Signal i a -> Packed i a
-    go (WE)     s = s
+    go (WE)     s = Sig s
     go (WP l r) s = (pack (Left s), pack (Right s))
     
 unpack :: forall i a. Witness a => Packed i a -> Signal i a
 unpack s = go (wit :: Wit a) s
   where
     go :: Wit a -> Packed i a -> Signal i a
-    go (WE)     s = s
+    go (WE)     s = runSig s
     go (WP l r) s = Join (unpack (fst s)) (unpack (snd s))
 
 --------------------------------------------------------------------------------
 -- **
 
-type family Wrapped a :: *
-type instance Wrapped (Identity a) = a
-type instance Wrapped (a, b)       = (Wrapped a, Wrapped b)
-
-wrap :: forall a. Witness a => a -> Wrapped a
-wrap w = go (wit :: Wit a) w
-  where
-    go :: Wit a -> a -> Wrapped a
-    go (WE)     a = runIdentity a
-    go (WP l r) a = (wrap (fst a), wrap (snd a))
-
-unwrap :: forall a. Witness a => Wrapped a -> a
-unwrap w = go (wit :: Wit a) w
-  where
-    go :: Wit a -> Wrapped a -> a 
-    go (WE)     a = Identity a
-    go (WP l r) a = (unwrap (fst a), unwrap (snd a))
+type family F (e :: * -> *) a :: *
+type instance F e (Identity a) = e a
+type instance F e (a, b) = (F e a, F e b)
 
 --------------------------------------------------------------------------------
 -- **
 
-lift :: forall proxy i a b.
-       ( Witness a, Typeable a
-       , Witness b, Typeable b
-       )
-    => proxy i a b
-    -> (Wrapped  a -> Wrapped  b)
-    -> (Packed i a -> Packed i b)
-lift _ f = pack . Map (stream f :: Stream i a -> Stream i b) . unpack
-
-stream :: forall i a b.
-    ( Witness a
-    , Witness b
-    )
-    => (Wrapped  a -> Wrapped  b)
-    -> (Stream i a -> Stream i b)
-stream f (Stream s) = Stream $ fmap (fmap (unwrap . f . wrap)) s
-
---------------------------------------------------------------------------------
-
-{-
-zipWith :: (a -> b -> c) -> Signal a -> Signal b -> Signal c
-zipWith f = curry $ liftAPA $ uncurry f
-
-liftAPA :: ((a, b) -> c) -> (Signal a, Signal b) -> Signal c
-liftAPA = undefined
--}
---------------------------------------------------------------------------------
-{-
-type family Instr a :: (* -> *) -> * -> *
-type instance Instr (Sig    instr a) = instr
-type instance Instr (Signal instr a) = instr
-type instance Instr (Empty  instr a) = instr
-type instance Instr (a, b)           = Instr a
-
--- | Conversion between signals and tuples
-class Syntax a
-  where
-    type Internal a :: *
-    
-    sugar   :: a -> Signal (Instr a) (Internal a)
-    desugar :: Signal (Instr a) (Internal a) -> a
-
---------------------------------------------------------------------------------
-
-instance Syntax (Sig instr a)
-  where
-    type Internal (Sig instr a) = Empty instr a
-
-    sugar   = unSig
-    desugar = Sig
-
-instance Syntax (Signal instr (Empty instr a))
-  where
-    type Internal (Signal instr (Empty instr a)) = Empty instr a
-
-    sugar   = id
-    desugar = id
-
-instance ( Syntax a
-         , Syntax b
-
-         , Instr a ~ Instr b
-         , Instr a ~ Instr (Internal a)
-         , Instr b ~ Instr (Internal b)
-{-
-         , Rep (Internal a)
-         , Rep (Internal b)
--}           
-         , Typeable (IExp (Instr a))
-         , Typeable (IExp (Instr b))
-         , Typeable (Internal a)
-         , Typeable (Internal b)
-         )
-    => Syntax (a, b)
-  where
-    type Internal (a, b) = (Internal a, Internal b)
-
-    sugar   (a, b) = Join (sugar a) (sugar b)
-    desugar s      = (desugar (Left s), desugar (Right s))
-
---------------------------------------------------------------------------------
--- **
-
--- | ...
-lift :: ( Syntax a
-        , Syntax b
-
-        , Instr a ~ Instr b
-        , Instr (Internal a) ~ Instr a
-        , Instr (Internal b) ~ Instr b
-
-        , Typeable (Internal a)
-        , Typeable (Internal b)
+lift :: forall proxy i e a b.
+        ( Witness a, Typeable a
+        , Witness b, Typeable b
+        , e ~ IExp i
         )
-     => (Internal a -> Internal b) -> a -> b
-lift f = desugar . Map (S.lift1 f) . sugar
--}
---------------------------------------------------------------------------------
--- *
---------------------------------------------------------------------------------
-{-
--- | ..
-class Rep a
+     => proxy i a b
+     -> (F e a      -> F e b)
+     -> (Packed i a -> Packed i b)
+lift _ f = pack . h . unpack
   where
-    represent :: Nested (Instr a) a -> Suple (Instr a) a
--}
---------------------------------------------------------------------------------
-{-
-instance (VarPred (IExp instr) a, Typeable a) => Rep (Empty instr a)
-  where
-    represent _ = Seaf ""
+    g = stream f       :: Stream i (F e a) -> Stream i (F e b)
+    h = Map (stream f) :: Signal i a       -> Signal i b
 
-instance (Instr a ~ Instr b, Rep a, Rep b) => Rep (a, b)
-  where
-    represent (_ :: Nested (Instr a) (a, b)) = Sranch (represent left) (represent right)
-      where
-        left  = undefined :: Nested (Instr a) a
-        right = undefined :: Nested (Instr b) b
--}
+stream :: (a -> b) -> (Stream i a -> Stream i b)
+stream f (Stream s) = Stream $ fmap (fmap f) s
+
 --------------------------------------------------------------------------------
--- ** 
-{-
--- | ...
-rep :: forall c instr a. (Rep a, Instr a ~ instr) => c instr a -> Suple instr a
-rep _ = represent (error "rep" :: Nested instr a)
--}
--- Frontend/Stream.hs
+++ Frontend/Stream.hs
@@ -1,12 +1,14 @@
-{-# LANGUAGE GADTs            #-}
-{-# LANGUAGE KindSignatures   #-}
+{-# LANGUAGE GADTs          #-}
+{-# LANGUAGE KindSignatures #-}
 
 module Frontend.Stream where
 
 import Core
+import Backend.Nested
+
 import Control.Applicative
 import Control.Monad
-import Prelude (($))
+import Prelude ((.), ($))
 
 --------------------------------------------------------------------------------
 -- * Streams
@@ -21,21 +23,24 @@ data Stream (instr :: (* -> *) -> * -> *) (a :: *)
 type Str instr a = Stream instr (IExp instr a)
 
 --------------------------------------------------------------------------------
+-- **
 
 -- | ...
 repeat :: (e ~ IExp instr) => e a -> Str instr a
-repeat a = Stream $ return $ return a
+repeat = Stream . return . return
 
 -- | ...
 map :: (e ~ IExp instr) => (e a -> e b) -> Str instr a -> Str instr b
-map f (Stream init) = Stream $ do
-  next <- init
-  return $ do
-    value <- next
-    return $ f value
+map f (Stream s) = Stream $ fmap (fmap f) s
 
 --------------------------------------------------------------------------------
+-- **
+
+stream :: Program instr (IExp instr a) -> Stream instr (IExp instr a)
+stream prg = Stream $ return prg
 
 -- | Run stream to produce transition action
 run :: Stream instr a -> Program instr a
 run (Stream init) = join init
+
+--------------------------------------------------------------------------------
