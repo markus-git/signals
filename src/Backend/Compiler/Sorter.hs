53d22ed8f06920a861e65a488fc436d59ac7b0a4
Markus <mararon@chalmers.se>
2015-07-01 15:13:34 +0200

    Sorter now uses Ref's

--- src/Backend/Compiler/Sorter.hs
+++ src/Backend/Compiler/Sorter.hs
@@ -1,47 +1,48 @@
 {-# LANGUAGE GADTs               #-}
 {-# LANGUAGE TypeOperators       #-}
+{-# LANGUAGE KindSignatures      #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 
 module Backend.Compiler.Sorter (
     Order
+  , Ordering
   , sorter
   )
 where
 
 import Frontend.Signal hiding (lift)
+import Frontend.Signal.Observ
   
 import Control.Arrow
 import Control.Monad.State
-import Data.Ref
-import Data.Unique
 import Data.List (sort)
+import Data.Ref
+import Data.Ref.Map (Map, Name)
 
-import           Data.Map (Map, (!))
-import qualified Data.Map as M
+import qualified Data.Ref.Map as M
 
-import Prelude hiding (Left, Right)
+import Prelude hiding (Left, Right, Ordering)
 
 --------------------------------------------------------------------------------
 -- * Sorter
 --------------------------------------------------------------------------------
 
 -- | During the sorting process a node can either be sorted or unvisited 
-data Status   = Visited | Unvisited deriving Eq
+data Status     = Visited | Unvisited deriving Eq
+
+data Tagged i a = Tagged Status Order (Node i a)
 
 -- | The ordering assigned to a node after being sorted
-type Order    = Int
+type Order      = Int
 
 -- | ...
-type Mapping  = Map Unique (Status, Order)
-
--- | ...
-type M = State (Order, Mapping)
+type M i = State (Order, Map (Tagged i))
 
 --------------------------------------------------------------------------------
 -- **
 
 -- | Returns a new and unique ordering
-new :: M Order
+new :: M i Order
 new = do
   (i, m) <- get
   put (i + 1, m)
@@ -51,45 +52,57 @@ new = do
 -- **
 
 -- | Updates the order of a node
-tag :: Unique -> Order -> M ()
-tag u o = modify $ second $ flip M.adjust u $ \(s, _) -> (s, o)
+tag :: Name a -> Order -> M i ()
+tag r o = modify $ second $ flip M.adjust r $ \(Tagged s _ n) -> Tagged s o n
 
--- | Updates the status of a node
-mark :: Unique -> Status -> M ()
-mark u s = modify $ second $ M.insert u (s, undefined)
+-- | Marks a node as visited
+visited :: Name a -> M i ()
+visited r = modify $ second $ flip M.adjust r $ \(Tagged _ o n) -> Tagged Visited o n
 
 -- | Gets the status of a node
-status :: Unique -> M Status
-status u = gets (fst . (! u) . snd)
+status :: Name a -> M i Status
+status r = gets $ (\(Tagged s _ _) -> s) . (M.! r) . snd
+
+-- | ...
+node :: Name (S Symbol i a) -> M i (S Key i a)
+node r = gets $ (\(Tagged _ _ (Node n)) -> n) . (M.! r) . snd
 
 --------------------------------------------------------------------------------
 -- *
 --------------------------------------------------------------------------------
 
--- | Given a root and a set of graph nodes, a topological ordering is produced
-sorter :: Sig i a -> [(Unique, Order)]
-sorter (Sig (Signal sym)) =
-  fmap (fmap snd) $ M.toList $ snd $ flip execState (0, M.empty) $ sort' sym
+data Ordering (i :: (* -> *) -> * -> *) a = Ordering Order
 
+-- | Given a root and a set of graph nodes, a topological ordering is produced
+sorter :: Key i a -> Map (Node i) -> Map (Ordering i)
+sorter (Key n) nodes = reduce $ snd $ flip execState (0, init nodes) $ sort' n
+--  fmap (fmap snd) $ M.toList $ snd $ flip execState (0, M.empty) $ sort' sym
+  where
+    init :: Map (Node i) -> Map (Tagged i)
+    init = M.hmap $ \node -> Tagged Unvisited 0 node
+
+    reduce :: Map (Tagged i) -> Map (Ordering i)
+    reduce = M.hmap $ \(Tagged _ o _) -> Ordering o
+    
 --------------------------------------------------------------------------------
 
--- | Standard depth-first ordering of a graph
-sort' :: Symbol i a -> M ()
-sort' (Symbol (Ref u sym)) = do
-    u `mark` Visited
-    b <- case sym of
+sort' :: Name (S Symbol i a) -> M i ()
+sort' r =
+  do visited r
+     n <- node r
+     case n of
        (Repeat  _) -> return ()
        (Map   _ s) -> visit s
        (Join  l r) -> visit l >> visit r
-       (Left    s) -> visit s
-       (Right   s) -> visit s
+       (Left    l) -> visit l
+       (Right   r) -> visit r
        (Delay _ s) -> visit s
-    o <- new
-    tag u o
+     o <- new
+     r `tag` o
   where
-    visit :: Symbol i a -> M ()
-    visit sym@(Symbol (Ref u _)) = do
-      v <- status u
-      when (v /= Visited) $ sort' sym
+    visit :: Key i a -> M i ()
+    visit (Key k) =
+      do s <- status k
+         when (s /= Visited) (sort' k)
 
 --------------------------------------------------------------------------------
