ac68216fc3f436ba34919cf9aed2d80b933d6164
Markus <mararon@chalmers.se>
2015-08-10 15:52:31 +0200

    linker makes use of knots again

--- src/Backend/Compiler/Linker.hs
+++ src/Backend/Compiler/Linker.hs
@@ -5,14 +5,14 @@
 {-# LANGUAGE FlexibleContexts    #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 
-module Backend.Compiler.Linker {-(
+module Backend.Compiler.Linker (
     Dist
   , Named(..)
   , Names
   , Link(..)
   , Linked(..)
   , linker
-  )-}
+  )
   where
 
 import Core
@@ -20,11 +20,13 @@ import Frontend.Stream
 import Frontend.Signal
 import Frontend.Signal.Observ
 import Backend.Knot
+import Backend.Compiler.Sorter
 
 import Control.Monad.Reader
 import Control.Monad.Writer
 import Control.Monad.State
 import Control.Monad.Identity
+import Data.Hashable
 import Data.Ref
 import Data.Ref.Map (Map, Name)
 import qualified Data.Ref.Map as M
@@ -49,6 +51,13 @@ data Named a
     Lefty  :: Named (S sym i (a, b)) -> Named (S sym i a)
     Righty :: Named (S sym i (a, b)) -> Named (S sym i b)
 
+instance Hashable (Named a)
+  where
+    hashWithSalt s (Named n)  = s `hashWithSalt` n
+    hashWithSalt s (Lefty l)  = s `hashWithSalt` (0 :: Int) `hashWithSalt` l
+    hashWithSalt s (Righty r) = s `hashWithSalt` (1 :: Int) `hashWithSalt` r
+
+-- | ...
 type Names a = Dist Named a
 
 -- | Takes a composite name and creates unique names for each part
@@ -60,7 +69,8 @@ name n = go (wit :: Wit a) (Named n)
     go (WP l r) n = (go l (Lefty n), go r (Righty n))
 
 --------------------------------------------------------------------------------
--- **
+-- *
+--------------------------------------------------------------------------------
 
 -- | ...
 data Link   (i :: (* -> *) -> * -> *) (a :: *)
@@ -85,11 +95,20 @@ unsafeReveal :: Hide f -> f a
 unsafeReveal (Hide f) = unsafeCoerce f
 
 --------------------------------------------------------------------------------
--- **
+-- *
+--------------------------------------------------------------------------------
 
-type Resolution i = Map (Linked i)
+data Resolution i = Resolution {
+    res_input  :: Map (Linked i)
+  , res_output :: Map (Linked i)
+  }
 
-type Constraint i = Hide (Pair Name (Linked i))
+data Constraint i = In  (Item i)
+                  | Out (Item i)
+
+--------------------------------------------------------------------------------
+
+type Item       i = Hide (Pair Name (Linked i))
 
 type Mapping    i = Map (Node i)
 
@@ -97,70 +116,68 @@ type M i = Knot (Resolution i) (Constraint i) (State (Mapping i))
 
 --------------------------------------------------------------------------------
 
+node :: Name (S Symbol i a) -> M i (Node i (S Symbol i a))
+node n =
+  do out <- get
+     case M.lookup n out of
+       Nothing   -> error "Linker.node:lookup failed"
+       Just node -> return node
 
+resolve :: Name (S Symbol i a) -> M i (Link i a)
+resolve n =
+  do out <- asks res_output
+     case M.lookup n out of
+       Nothing           -> error "Linker.resolve:lookup failed"
+       Just (Linked _ l) -> return l
+
+input  :: Item i -> M i ()
+input  i = tell [In i]
+
+output :: Item i -> M i ()
+output i = tell [Out i]
 
 --------------------------------------------------------------------------------
 -- *
 --------------------------------------------------------------------------------
 
-
-
-
-
-
-
-
---------------------------------------------------------------------------------
--- * Linker
---------------------------------------------------------------------------------
-{-
-type M i = ReaderT (Map (Node i)) (State (Map (Linked i)))
-
-node :: Name (S Symbol i a) -> M i (S Key i a)
-node r = asks $ (\(Node n) -> n) . (M.! r)
-
-new  :: Name a -> Linked i a -> M i ()
-new r l = modify $ M.insert (Ref r undefined) l
-
-old  :: Key i a -> M i (Link i a)
-old (Key k) = gets $ Link . (\(Linked _ n) -> n) . (M.! k)
--}
---------------------------------------------------------------------------------
--- **
-{-
--- | ...
 link' :: Ordered i -> M i ()
 link' (Ordered sym) =
   do let out = name sym
-     n <- node sym
+     (Node n) <- node sym
      case n of
-       (Repeat c) -> do
-         new sym $ Linked (Repeat c) out
-       (Map  f s) -> do
-         ms <- old s
-         new sym $ Linked (Map f ms) out
-       (Join l r) -> do
-         ml@(Link p) <- old l
-         mr@(Link q) <- old r
-         new sym $ Linked (Join ml mr) (p, q)
-       (Left   l) -> do
-         ml@(Link (p, _)) <- old l
-         new sym $ Linked (Left ml) p
-       (Right  r) -> do
-         mr@(Link (_, q)) <- old r
-         new sym $ Linked (Right mr) q
-       (Delay v s) -> do
-         ms <- old s
-         new sym $ Linked (Delay v ms) out
+       (Repeat c) ->
+         do output $ Hide $ Pair sym (Linked (Repeat c) (Link out))
+       (Map f (Key s)) ->
+         do inp <- resolve s
+            output $ Hide $ Pair sym (Linked (Map f inp) (Link out))
+       (Join (Key l) (Key r)) ->
+         do inp_l@(Link l') <- resolve l
+            inp_r@(Link r') <- resolve r
+            output $ Hide $ Pair sym (Linked (Join inp_l inp_r) (Link (l', r')))
+       (Left (Key l)) ->
+         do inp_l@(Link l') <- resolve l
+            output $ Hide $ Pair sym (Linked (Left inp_l) (Link (fst l')))
+       (Right (Key r)) ->
+         do inp_r@(Link r') <- resolve r
+            output $ Hide $ Pair sym (Linked (Right inp_r) (Link (snd r')))
+       (Delay d (Key s)) ->
+         do inp <- resolve s
+            output $ Hide $ Pair sym (Linked (Delay d inp) (Link out))
 
--}
---------------------------------------------------------------------------------
--- **
-{-
--- | ...
 linker :: [Ordered i] -> Map (Node i) -> Map (Linked i)
-linker order nodes = flip execState M.empty
-                   . flip runReaderT nodes
-                   $ forM_ order link'
--}
+linker order nodes = undefined
+  where
+    solve :: Solver (Resolution i) (Constraint i)
+    solve constraints =
+      let inputs  = [ i | In  i <- constraints]
+          outputs = [ o | Out o <- constraints]
+      in  Resolution {
+              res_output = list outputs
+            , res_input  = list  inputs
+          }
+
+    list :: [Item i] -> Map (Linked i)
+    list = foldr (\(Hide (Pair n l)) m -> M.insert (ref n) l m) M.empty
+      where ref = flip Data.Ref.Ref undefined
+
 --------------------------------------------------------------------------------
