ac51d44094375123d14fce16a2148518c55f4d28
Markus <mararon@chalmers.se>
2015-08-24 13:40:08 +0200

    filled in main module

--- src/Signal.hs
+++ src/Signal.hs
@@ -1,1 +1,9 @@
-module Signal where
+module Signal
+  ( module Signal.Sig
+  , module Signal.Str
+  , module Signal.Compiler
+  ) where
+
+import Signal.Core        as Signal.Sig hiding (Symbol, S, U, Wit, Witness)
+import Signal.Core.Stream as Signal.Str hiding (map, repeat)
+import Signal.Compiler
