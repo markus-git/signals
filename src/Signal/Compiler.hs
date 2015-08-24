1c3866400f7dcf7cf612a326f11906ea0438181e
Markus <mararon@chalmers.se>
2015-08-24 11:24:46 +0200

    compiler now accepts any compileable expr type, IExp

--- src/Signal/Compiler.hs
+++ src/Signal/Compiler.hs
@@ -23,8 +23,8 @@ import Signal.Compiler.Cycles
 import Signal.Compiler.Linker
 import Signal.Compiler.Sorter
 
-import Language.VHDL
-import Language.Embedded.VHDL
+import Language.VHDL          hiding (Name)
+import Language.Embedded.VHDL hiding (name)
 
 import Control.Arrow    (first, second)
 import Control.Monad.Reader
@@ -51,58 +51,62 @@ import Prelude hiding (lookup, Left, Right)
 -- * Channels
 --------------------------------------------------------------------------------
 
--- ! generalize ! Expr a --> IExp i a
-
 data Hide f where
   Hide :: f a -> Hide f
 
-data Channels (i :: (* -> *) -> * -> *) = Channels (Map.IntMap (Hide (IExp i)))
+type Channels (i :: (* -> *) -> * -> *) = Map.IntMap Identifier
 
-lookupC :: Named (S Symbol i (Identity a)) -> Channels i -> IExp i a
-lookupC n (Channels m) = case Map.lookup (hash n) m of
-  Nothing       -> error "Compiler.lookupC: lookup failed"
-  Just (Hide x) -> unsafeCoerce x
+lookupC :: Named (S Symbol i (Identity a)) -> Channels i -> Identifier
+lookupC n m = case Map.lookup (hash n) m of
+  Nothing -> error "Compiler.lookupC: lookup failed"
+  Just i  -> i
 
-insertC :: Named (S Symbol i (Identity a)) -> IExp i a -> Channels i -> Channels i
-insertC n e (Channels m) = Channels $ Map.insert (hash n) (Hide e) m
+lookupE
+  :: (CompileExp (IExp i), PredicateExp (IExp i) a)
+  => Named (S Symbol i (Identity a))
+  -> Channels i
+  -> IExp i a
+lookupE n = varE . lookupC n
+
+insertC :: Named (S Symbol i (Identity a)) -> Identifier -> Channels i -> Channels i
+insertC n i m = Map.insert (hash n) i m
+
+initC   :: forall i. CompileExp (IExp i) => Rim.Map (Linked i) -> Channels i
+initC m = let links = fmap snd . concat $ Rim.dump m in
+    snd $ flip execState (1, Map.empty) $ forM_ links add
+  where
+    add :: Rim.HideType (Linked i) -> Init i ()
+    add (Rim.Hide (Linked _ l@(Link out))) = insert l out
 
 --------------------------------------------------------------------------------
 
 type Init i = State (Integer, Channels i)
 
-new :: (CompileExp (IExp i), PredicateExp (IExp i) a) => Init i (IExp i a)
+new :: Init i Identifier
 new = 
   do i <- gets fst
      modify $ first (+ 1)
      return $ newVar i
-{-
+
 insert
-  :: forall proxy i a. (CompileExp (IExp i), Witness a)
+  :: forall proxy i a. (CompileExp (IExp i), Witness i a)
   => proxy i a
   -> Names (S Symbol i a)
   -> Init i ()
-insert _ n = go (wit :: Wit a) n
+insert _ n = go (wit :: Wit i a) n
   where
-    go :: Wit x -> Names (S Symbol i x) -> Init i ()
+    go :: Wit i x -> Names (S Symbol i x) -> Init i ()
     go (WE)     (name) = add name
     go (WP u v) (l, r) = go u l >> go v r
 
     add :: PredicateExp (IExp i) x => Named (S Symbol i (Identity x)) -> Init i ()
-    add name = new >>= \i -> modify $ second $ insertC name i
--}{-
-chan_init   :: Rim.Map (Linked i) -> Channels
-chan_init m =
-  let links = fmap snd . concat $ Rim.dump m
-   in snd $ flip execState (1, Map.empty) $ forM_ links add
-  where
-    add :: Rim.HideType (Linked i) -> Init ()
-    add (Rim.Hide (Linked _ l@(Link out))) = insert l out
--}
+    add name = new >>= modify . second . insertC name
+
 --------------------------------------------------------------------------------
 -- *
 --------------------------------------------------------------------------------
-{-
-type M i = ReaderT (Rim.Map (Linked i)) (StateT Channels (Program i))
+
+type M i = ReaderT (Rim.Map (Linked i)) (StateT (Channels i) (Program i))
 
 node :: Name (S Symbol i a) -> M i (Linked i (S Symbol i a))
 node name =
@@ -111,50 +115,52 @@ node name =
        Nothing   -> error "Compiler.node: lookup failed"
        Just node -> node
 
-read_exp  :: forall proxy i a. (IExp i ~ Expr, Witness a)
-          => proxy i a
-          -> Names (S Symbol i a)
-          -> M i (U i a)
-read_exp _ n = go (wit :: Wit a) n
+readE
+  :: forall proxy i a. (ConcurrentCMD (IExp i) :<: i, CompileExp (IExp i), Witness i a)
+  => proxy i a
+  -> Names (S Symbol i a)
+  -> M i (U i a)
+readE _ n = go (wit :: Wit i a) n
   where
-    go :: Wit x -> Names (S Symbol i x) -> M i (U i x)
-    go (WE)     (name) = gets $ chan_lookup name
+    go :: Wit i x -> Names (S Symbol i x) -> M i (U i x)
+    go (WE)     (name) = gets $ lookupE name
     go (WP u v) (l, r) =
       do l' <- go u l
          r' <- go v r
          return (l', r')
 
-write_exp :: forall proxy i a. (ConcurrentCMD (IExp i) :<: i, IExp i ~ Expr, Witness a)
-          => proxy i a
-          -> Names (S Symbol i a)
-          -> U i a
-          -> M i ()
-write_exp _ n e = go (wit :: Wit a) n e
+writeE
+  :: forall proxy i a. (ConcurrentCMD (IExp i) :<: i, Witness i a)
+  => proxy i a
+  -> Names (S Symbol i a)
+  -> U i a
+  -> M i ()
+writeE _ n e = go (wit :: Wit i a) n e
   where
-    go :: Wit x -> Names (S Symbol i x) -> U i x -> M i ()
+    go :: Wit i x -> Names (S Symbol i x) -> U i x -> M i ()
     go (WP u v) (l, r) (a, b) = go u l a >> go v r b
     go (WE)     (name) (expr) =
-      do c <- gets $ chan_find name
-         lift $ lift $ c <=: expr
--}
+      do c <- gets $ lookupC name
+         lift $ lift $ c <== expr
+
 --------------------------------------------------------------------------------
-{-
-comp' :: (ConcurrentCMD (IExp i) :<: i, IExp i ~ Expr) => Ordered i -> M i ()
+
+comp' :: (ConcurrentCMD (IExp i) :<: i, CompileExp (IExp i)) => Ordered i -> M i ()
 comp' (Ordered sym) =
   do (Linked n olink@(Link out)) <- node sym
      case n of
        (Repeat c) ->
          do v <- down c
-            write_exp olink out v
+            writeE olink out v
        (Map f ilink@(Link s)) ->
-         do e <- read_exp ilink s
-            v <- up       ilink e
+         do e <- readE ilink s
+            v <- up    ilink e
             let o = f v
             y <- down o
-            write_exp olink out y
+            writeE olink out y
        (Delay v ilink@(Link s)) ->   -- ! This is clearly wrong
-         do e <- read_exp ilink s
-            write_exp olink out v
+         do e <- readE ilink s
+            writeE olink out v
        _ -> return ()
   where
     down :: Stream i a -> M i a
@@ -162,33 +168,43 @@ comp' (Ordered sym) =
 
     up   :: forall proxy i a. proxy i a -> U i a -> M i (Stream i (U i a))
     up _ = return . Str.Stream . return . return 
--}    
+
 --------------------------------------------------------------------------------
-{-
-compile' :: forall i a. (ConcurrentCMD (IExp i) :<: i, IExp i ~ Expr, Typeable a)
-         => Key i (Identity a)
-         -> Rim.Map (Node i)
-         -> Rim.Map (Linked i)
-         -> [Ordered i]
-         -> Str i a
+
+compile'
+  :: forall i a.
+     ( ConcurrentCMD (IExp i) :<: i
+     , CompileExp (IExp i)
+     , PredicateExp (IExp i) a
+     , Typeable a
+     )
+  => Key i (Identity a)
+  -> Rim.Map (Node i)
+  -> Rim.Map (Linked i)
+  -> [Ordered i]
+  -> Str i a
 compile' k@(Key root) nodes links order = Stream $ 
-  do let ch = chan_init links
+  do let ch = initC links
      run ch $ do
        mapM_ comp' order
-       out <- read_exp k (name root)
+       out <- readE k (name root)
        ret out
   where
-    run :: Channels -> M i (Expr a) -> Program i (Program i (Expr a))
+    run :: Channels i -> M i (IExp i a) -> Program i (Program i (IExp i a))
     run c = return . flip evalStateT c . flip runReaderT links
 
-    ret :: U i (Identity a) -> M i (Expr a)
+    ret :: U i (Identity a) -> M i (IExp i a)
     ret = return
--}
+
 --------------------------------------------------------------------------------
-{-
-compiler :: (ConcurrentCMD (IExp i) :<: i, IExp i ~ Expr, Typeable a)
-         => Sig i a
-         -> IO (Str i a)
+
+compiler
+  :: ( ConcurrentCMD (IExp i) :<: i
+     , CompileExp (IExp i)
+     , PredicateExp (IExp i) a
+     , Typeable a
+     )
+  => Sig i a -> IO (Str i a)
 compiler sig =
   do (root, nodes) <- reify sig
 
@@ -199,5 +215,5 @@ compiler sig =
      return $ case cycle of
        True  -> error "Compiler.compiler: found cycle"
        False -> compile' root nodes links order
--}
+
 --------------------------------------------------------------------------------
