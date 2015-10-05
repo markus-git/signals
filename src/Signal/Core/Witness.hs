ea83bdd079858500b4ef1a7e7aa28fe1de340a68
Markus <mararon@chalmers.se>
2015-10-05 16:43:39 +0200

    comments for Witness class/data

--- src/Signal/Core/Witness.hs
+++ src/Signal/Core/Witness.hs
@@ -16,11 +16,10 @@ import Language.Embedded.VHDL (PredicateExp)
 -- * Witness
 --------------------------------------------------------------------------------
 
--- | ...
+-- | A witness for the correct construction (as a nested tuple) of some type
 data Wit (i :: (* -> *) -> * -> *) a
   where
-    WE :: (Typeable a, PredicateExp (IExp i) a)
-       => Wit i (Identity a)
+    WE :: (Typeable a, PredicateExp (IExp i) a) => Wit i (Identity a)
           
     WP :: (Witness i a, Witness i b)
        => Wit i a
@@ -30,17 +29,17 @@ data Wit (i :: (* -> *) -> * -> *) a
 --------------------------------------------------------------------------------
 -- ** ...
 
--- | ...
+-- | Class of things for which we can produce a correctness witness
 class Typeable a => Witness i a
   where
     witness :: Typeable a => Wit i a
 
--- | ...
+-- | Single value case
 instance (Typeable a, PredicateExp (IExp i) a) => Witness i (Identity a)
   where
     witness = WE
 
--- | ...
+-- | Nested tuple case
 instance (Witness i a, Witness i b) => Witness i (a, b)
   where
     witness = WP (witness :: Wit i a) (witness :: Wit i b)
