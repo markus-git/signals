a320bc013c8c7b3fe4b088440642fcde73644e44
Markus <mararon@chalmers.se>
2015-10-06 17:06:58 +0200

    wrote a mux. example

--- examples/Filters.hs
+++ examples/Filters.hs
@@ -4,12 +4,14 @@ module Filters where
 
 import Language.VHDL
 import Language.Embedded.VHDL
-import Language.Embedded.VHDL.Expression
-import Control.Monad.Operational.Compositional
+import qualified Language.Embedded.VHDL.Expression as E
 
 import Signal hiding (E)
+import Signal.Compiler.Interface
 
+import Control.Monad.Operational.Compositional
 import Data.Bits
+import Data.Typeable
 import Data.Word
 import System.IO
 
@@ -32,9 +34,15 @@ type S = Sig (CMD E)
 -- | ...
 type Sr a = Str (CMD E) a
 
---------------------------------------------------------------------------------
+-- | ...
+instance Compile Expr
+  where
+    literal = litE
 
-repeat :: E Word8 -> S Word8
+--------------------------------------------------------------------------------
+-- ** Some simple signal functions
+
+repeat :: (Rep a, Typeable a) => E a -> S a
 repeat = lift0
 
 map :: (E Word8 -> E Word8) -> S Word8 -> S Word8
@@ -44,6 +52,23 @@ zipWith :: (E Word8 -> E Word8 -> E Word8) -> S Word8 -> S Word8 -> S Word8
 zipWith f = lift2 f
 
 --------------------------------------------------------------------------------
+-- ** Some simple circuits
+
+high, low :: E Bool
+high = litE True
+low  = litE False
+
+inv :: S Bool -> S Bool
+inv = lift1 E.not
+
+multiplexer :: S Word8 -> S Word8
+multiplexer sig = mux2 toggle (sig, zeroes)
+  where
+    zeroes = repeat 0
+    toggle = repeat high --high `delay` (inv toggle)
+
+--------------------------------------------------------------------------------
+-- ** Some filters
 
 divs :: S Word8 -> S Word8 -> S Word8
 divs = zipWith div
@@ -57,14 +82,13 @@ muls es = P.zipWith (*) (P.map repeat es)
 delays :: [E Word8] -> S Word8 -> [S Word8]
 delays es s = scanl (flip delay) s es
 
---------------------------------------------------------------------------------
--- ** Some filters
-
+-- | Finite impulse response filter
 fir :: [E Word8] -> S Word8 -> S Word8
 fir as = sums . muls as . delays ds
   where
     ds = P.replicate (P.length as) 0
 
+-- | Infinite impulse response filter
 iir :: [E Word8] -> [E Word8] -> S Word8 -> S Word8
 iir (a:as) bs s = o
   where
@@ -85,11 +109,17 @@ compSF f =
 
 --------------------------------------------------------------------------------
 
-test :: IO ()
-test =
-  do prog <- compSF (fir [1,2])
-     putStrLn $ compile prog
+testMux :: IO ()
+testMux = compSF (multiplexer) >>= putStrLn . compile
 
+testFIR :: IO ()
+testFIR = compSF (fir [1,2]) >>= putStrLn . compile
+
+testIIR :: IO ()
+testIIR = compSF (iir [1,2] [3,4]) >>= putStrLn . compile
+
+--------------------------------------------------------------------------------
+-- *
 --------------------------------------------------------------------------------
 
 write :: IO ()
