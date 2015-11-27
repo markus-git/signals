a3d89af3a9ceb3e0a7c7c4de68ace6fe52b12ce9
Markus <mararon@chalmers.se>
2015-11-27 21:45:54 +0100

    removed reify running text

--- src/Signal/Core/Reify.hs
+++ src/Signal/Core/Reify.hs
@@ -35,12 +35,9 @@ import Prelude hiding (Left, Right, join)
 -- * Reification of Signals
 --------------------------------------------------------------------------------
 
-{--- | Index type for names
-type Ix i a = Name (S Symbol i a)
--}
 -- | Index keys of a reification mapping
 data Key (i :: (* -> *) -> * -> *) (a :: *) where
-  Key ::  Name (S Symbol i a) -> Key i a
+  Key :: Witness i a => Name (S Symbol i a) -> Key i a
 
 -- | Values of a reification mapping
 data Node (i :: (* -> *) -> * -> *) (a :: *) where
@@ -52,28 +49,29 @@ type Nodes i = Map (Node i)
 --------------------------------------------------------------------------------
 -- ** Reification functions
 
-reify :: Sig i a -> IO (Key i (Identity a), Nodes i)
-reify s = do
-  x <- reifyy s
-  putStrLn "Reify done!"
-  return x
-
 -- | Reification of a signal into a mapping over its nodes and root key
-reifyy :: Sig i a -> IO (Key i (Identity a), Nodes i)
-reifyy (Sig (Signal sym)) =
+reify
+  :: ( PredicateExp (IExp i) a
+     , Typeable a)
+  => Sig i a
+  -> IO ( Key   i (Identity a)
+        , Nodes i
+        )
+reify (Sig (Signal sym)) =
   second fst <$> runStateT (reify' sym) (M.empty, M.empty)
 
 -- | Reification of a signal function into a mapping over its nodes, root key and input key
 freify
   :: ( PredicateExp (IExp i) a
+     , PredicateExp (IExp i) b
      , Typeable i
      , Typeable a
      , Typeable b)
   => (Sig i a -> Sig i b)
   -> IO (Key i (Identity b), Nodes i)
 freify f =
-  do let (_, graph) = let a = Sig (S.variable (toDyn f)) in (a, f a)
-     reify graph
+  let (_, graph) = let a = Sig (S.variable (toDyn f)) in (a, f a)
+   in reify graph
 
 --------------------------------------------------------------------------------
 -- ** ...
@@ -82,7 +80,7 @@ freify f =
 type Reify i = StateT (Nodes i, Map Name) IO
 
 -- | Reification of a symbol tree
-reify' :: forall i a. Symbol i a -> Reify i (Key i a)
+reify' :: forall i a. Witness i a => Symbol i a -> Reify i (Key i a)
 reify' (Symbol ref@(Ref k node)) =
   do name <- lookupName ref
      case name of
@@ -118,7 +116,7 @@ reify' (Symbol ref@(Ref k node)) =
 --------------------------------------------------------------------------------
 
 -- | Insert a signal node under the given reference name
-insertNode :: Ref (S Symbol i a) -> Node i (S Symbol i a) -> Reify i (Key i a)
+insertNode :: Witness i a => Ref (S Symbol i a) -> Node i (S Symbol i a) -> Reify i (Key i a)
 insertNode ref@(Ref name _) node = modify (first $ M.insert ref node) >> return (Key name)
 
 -- | Insert a reference name
@@ -126,7 +124,7 @@ insertName :: Ref (S Symbol i a) -> Reify i ()
 insertName ref@(Ref name _) = modify $ second $ M.insert ref name
 
 -- | Tries to find a reference name
-lookupName :: Ref (S Symbol i a) -> Reify i (Maybe (Key i a))
+lookupName :: Witness i a => Ref (S Symbol i a) -> Reify i (Maybe (Key i a))
 lookupName ref@(Ref name _) = do
   node  <- gets (M.lookup name . snd)
   return $ case node of
