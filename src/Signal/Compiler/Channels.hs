78aef01ecb287e20a2113f1d310d030268e0920c
Markus <mararon@chalmers.se>
2015-11-27 10:13:46 +0100

    channels include identifier kind, i.e variable/signal

--- src/Signal/Compiler/Channels.hs
+++ src/Signal/Compiler/Channels.hs
@@ -19,6 +19,7 @@ module Signal.Compiler.Channels
 
 import Signal.Core            (S(..), E(..), Symbol)
 import Signal.Core.Witness
+import Signal.Core.Reify      (Key(..))
 import Signal.Compiler.Linker
 
 import Control.Monad.Operational.Higher
@@ -36,10 +37,13 @@ import qualified Data.Ref.Map as RMap
 import qualified Data.IntMap  as IMap
 
 import Language.VHDL                          (Identifier)
+import Language.Embedded.VHDL                 (Mode)
 import Language.Embedded.VHDL.Expression.Type (Kind)
 import qualified Language.VHDL                          as VHDL
 import qualified Language.Embedded.VHDL                 as HDL
 import qualified Language.Embedded.VHDL.Expression.Type as HDL
+
+import System.Mem.StableName (eqStableName)
 
 import Prelude hiding (Left, Right)
 
@@ -47,70 +51,96 @@ import Prelude hiding (Left, Right)
 -- * Compiler constructs
 --------------------------------------------------------------------------------
 
-newtype Channels = Channels { runChannels :: IMap.IntMap Identifier }
+data Scope   = Local | Global | Port
 
-lookup :: Name a -> Channels -> Maybe Identifier
+data Channel = Channel Identifier Kind
+
+newtype Channels = Channels { runChannels :: IMap.IntMap Channel }
+
+lookup :: Name a -> Channels -> Maybe Channel
 lookup name (Channels m) = IMap.lookup (hash name) m
 
-insert :: Name a -> Identifier -> Channels -> Channels
-insert name ident (Channels m) = Channels (IMap.insert (hash name) ident m)
+insert :: Name a -> Identifier -> Kind -> Channels -> Channels
+insert name i k (Channels m) = Channels (IMap.insert (hash name) (Channel i k) m)
 
 --------------------------------------------------------------------------------
 -- ** ...
 
 fromLinks
-  :: forall i.
+  :: forall i a.
      ( HDL.SequentialCMD (IExp i) :<: i
      , HDL.ConcurrentCMD (IExp i) :<: i
+     , HDL.HeaderCMD     (IExp i) :<: i
      )
-  => Links i
+  => Key i (Identity a)
+  -> Links i
   -> Program i Channels
-fromLinks = fromList . filter useful . RMap.elems
+fromLinks key = fromList key . filter useful . RMap.elems
 
 --------------------------------------------------------------------------------
 
 type M i = StateT Channels (Program i)
 
 fromList
-  :: forall i.
+  :: forall i a.
      ( HDL.SequentialCMD (IExp i) :<: i
      , HDL.ConcurrentCMD (IExp i) :<: i
+     , HDL.HeaderCMD     (IExp i) :<: i
      )
-  => [Entry (Linked i)]
+  => Key i (Identity a)
+  -> [Entry (Linked i)]
   -> Program i Channels
-fromList es = CMS.execStateT (mapM_ go es) (Channels IMap.empty)
+fromList (Key key) es =
+    CMS.execStateT (mapM_ go es) (Channels IMap.empty)
   where
     go :: Entry (Linked i)  -> M i ()
     go (RMap.Entry name (Linked node o@(Link n))) =
       case node of
+        Var d -> do
+          init o
         Repeat c -> do
-          declare n (Just c)
+          declare n HDL.Variable Local (Just c)
         Map f s -> do
           init o
         Delay d s -> do
-          declare n (Just d)
+          declare n HDL.Signal Global (Just d)
           -- ??? 'opposite' ???
           -- declare ? (Nothing)
         Mux s cs -> do
           init o
 
-    init :: forall a. Link i a -> M i ()
-    init (Link names) = dist (witness :: Wit i a) names
+    init :: forall b. Link i b -> M i ()
+    init (Link names) = dist (witness :: Wit i b) names
       where
         dist :: Wit i x -> Names (S Symbol i x) -> M i ()
-        dist (WE)     (name) = declare name Nothing
-        dist (WP l r) (u, v) = dist l u >> dist r v
+        dist (WE)     (name) =
+          do declare name HDL.Variable Local Nothing
+        dist (WP l r) (u, v) =
+          do dist l u
+             dist r v
 
--- | Declares a VHDL variable for a name with, possibly, some init value
-declare
-  :: ( HDL.SequentialCMD (IExp i) :<: i
-     , HDL.PredicateExp  (IExp i) a)
-  => Named (S sym i (Identity a))
-  -> Maybe (IExp i a)
-  -> M i ()
-declare (Named n) e = do
-  i <- CMS.lift $ HDL.variableL e
-  CMS.modify (insert n i)  
+    declare
+      :: forall b.
+         ( HDL.SequentialCMD (IExp i) :<: i
+         , HDL.ConcurrentCMD (IExp i) :<: i
+         , HDL.PredicateExp  (IExp i) b)
+      => Named (S Symbol i (Identity b))
+      -> Kind
+      -> Scope
+      -> Maybe (IExp i b)
+      -> M i ()
+    declare (Named n) kind scope e
+      | key `eqStableName` n =
+          do i <- CMS.lift $ HDL.signalPort HDL.In e
+             CMS.modify (insert n i HDL.Signal)
+      | otherwise =
+          do i <- CMS.lift $ case kind of
+               HDL.Variable -> case scope of
+                 Local  -> HDL.variableL e
+               HDL.Signal -> case scope of
+                 Port   -> HDL.signalPort HDL.Out e
+                 Global -> HDL.signalG e
+             CMS.modify (insert n i undefined)  
 
 -- | Usefulness refers to whether we should generate code for the node or not
 useful :: RMap.Entry (Linked i) -> Bool
